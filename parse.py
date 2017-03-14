0472fa17f4899c62fc73140057d18c028ca16c05
Aleksandar Milinkovic <amilinko@gmail.com>
2017-03-15 00:03:48 +0100

    Simplified parsing.

--- parse.py
+++ parse.py
@@ -1,38 +1,29 @@
-import collections
+import docopt
 import xmltodict
 from gzip import GzipFile
 
-def parse_sentence (sentence, column):
-	lines = sentence.split('\n')
-	parsed = []
-	for line in lines:
-		words = line.split()
-		if len(words) > 0:
-			word = words[column]
-			parsed.append(word)
-		
-	print " ".join(parsed).encode('utf-8')
+global column
+
+def parse_sentences(sentences, column):
+	for sentence in sentences:
+		text = sentence if type(sentence) is unicode else sentence['#text']
+		lines = text.split('\n')
+		parsed = [line.split()[column] for line in lines if line]
+		print " ".join(parsed).encode('utf-8')
 	
 def handle(path, item):
-	#print (path[1][1]['lang'])
+	lang = path[1][1]['lang']
+	sentences = item['s'] if lang == 'sr' else []
 	
-	sentence = item['s']
-	if type(sentence) is unicode:
-		parse_sentence (sentence,0)
+	if type(sentences) is not list:
+		sentences = [sentences]
 	
-	if type(sentence) is list:
-		for s in sentence:
-			if type(s) is unicode:
-				parse_sentence(s,0)
-			else:
-				parse_sentence (s['#text'],0)
-	
-	if type(sentence) is collections.OrderedDict:
-		parse_sentence(sentence['#text'],0)
-		
+	parse_sentences (sentences, column)
 	return True
 	
 file = '../srWaC/gz/srWaC1.1.01.xml.gz'
 #file = '../srWaC/gz/small.xml.gz'
 
-xmltodict.parse(GzipFile(file), item_depth=2, item_callback=handle)
+column = 0
+
+xmltodict.parse(GzipFile(file), item_depth=2, item_callback=handle, dict_constructor=dict)
