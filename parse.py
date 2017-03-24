03ec2c5cce57a92b837d6deefb907e988aaffe2c
Aleksandar Milinkovic <amilinko@gmail.com>
2017-03-24 17:13:12 +0100

    Changed parser. XML files are parsed line by line. Used standard Python libraries.

--- parse.py
+++ parse.py
@@ -1,33 +1,21 @@
-import sys
-import xmltodict
-from gzip import GzipFile
+import fileinput
+import HTMLParser
 
-global column
+is_serbian = False
+h = HTMLParser.HTMLParser()
 
-def parse_sentences(sentences, column):
-	for sentence in sentences:
-		text = sentence if type(sentence) is unicode else sentence['#text']
-		lines = text.split('\n')
-		parsed = [line.split()[column] for line in lines if line]
-		print " ".join(parsed).encode('utf-8')
-	
-def handle(path, item):
-	lang = path[1][1]['lang']
-	sentences = item['s'] if (lang == 'sr') and (item['s'] is not None) else []
-	
-	if type(sentences) is not list:
-		sentences = [sentences]
-	
-	parse_sentences (sentences, column)
-	return True
-
-# Parse command line arguments
-if len(sys.argv)<>3:
-	print "Usage: python parse.py <filename> <column>"
-	sys.exit(1)
-
-filename = sys.argv[1]
-column = int(sys.argv[2])
-
-# Parse in streaming mode
-xmltodict.parse(GzipFile(filename), item_depth=2, item_callback=handle, dict_constructor=dict)
+for line in fileinput.input():
+	if line.startswith('<p'):
+		is_serbian = 'lang="sr"' in line
+	elif line == '</p>':
+		is_serbian = False
+	elif is_serbian:
+		if line == '<s>\n':
+			sentence = []
+		elif line == '</s>\n':
+			if sentence:
+				print " ".join(sentence).encode('utf-8')
+		elif line.startswith('<'):
+			pass
+		else:
+			sentence.append(h.unescape(line.split('\t')[2].decode('utf-8')))
