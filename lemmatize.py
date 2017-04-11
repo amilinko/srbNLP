b2341accf9e5fcac448d2b72a17b8f5452b3a1cf
Aleksandar Milinkovic <amilinko@gmail.com>
2017-04-11 16:22:46 +0200

    Minor fixes in lemmatize.py.

--- lemmatize.py
+++ lemmatize.py
@@ -1,7 +1,7 @@
 """lemmatize.py
  
 Usage:
-  lemmatize.py btagger <InputFile> <OutputFile>
+  lemmatize.py BTagger <InputFile> <OutputFile>
   lemmatize.py -h | --help
   lemmatize.py --version
  
@@ -20,41 +20,52 @@ if __name__ == '__main__':
 	
 	import urllib
 	import os
+	import sys
+	
 	from subprocess import call
 	
 	inputFile = arguments["<InputFile>"]
 	outputFile = arguments["<OutputFile>"]
 	
 	# BTagger
-	if (arguments["btagger"]):
+	if (arguments["BTagger"]):
+		
+		# Constants
+		BTAGGER = "BTagger.jar"
+		PARAM = "param"
+		TMP = "tmp"
+		LEMMA_WEIGHT = PARAM + os.sep + "lem.fea"
+		LEMMA_SCRIPT = PARAM + os.sep + "lem.scr"
+		POS_WEIGHT = PARAM + os.sep + "pos.fea"
+		POS_SCRIPT = PARAM + os.sep + "pos.scr"
 		
 		# Check if Btagger.jar exists in the current directory
-		if(not os.path.isfile("BTagger.jar")):
+		if(not os.path.isfile(BTAGGER)):
 			print ("Downloading BTagger.jar...")
-			urllib.urlretrieve ("http://clcl.unige.ch/btag/BTagger.jar", "BTagger.jar")
+			urllib.urlretrieve ("http://clcl.unige.ch/btag/BTagger.jar", BTAGGER)
 			
 		# Check if predefined models for lemmatization and tagging are downloaded
-		if(not os.path.isdir("param")):
-			call(["mkdir", "param"])
+		if(not os.path.isdir(PARAM)):
+			call(["mkdir", PARAM])
 			
-		if(not os.path.isfile("param/lem.fea")):
-			print ("Downloading lea.fea...")
-			urllib.urlretrieve ("http://clcl.unige.ch/btag/param/sr/lem.fea", "param/lem.fea")
+		if(not os.path.isfile(LEMMA_WEIGHT)):
+			print ("Downloading lemmatisation weight file...")
+			urllib.urlretrieve ("http://clcl.unige.ch/btag/param/sr/lem.fea", LEMMA_WEIGHT)
 			
-		if(not os.path.isfile("param/lem.scr")):
-			print ("Downloading lem.scr...")
-			urllib.urlretrieve ("http://clcl.unige.ch/btag/param/sr/lem.scr", "param/lem.scr")
+		if(not os.path.isfile(LEMMA_SCRIPT)):
+			print ("Downloading lemmatisation script file...")
+			urllib.urlretrieve ("http://clcl.unige.ch/btag/param/sr/lem.scr", LEMMA_SCRIPT)
 			
-		if(not os.path.isfile("param/pos.fea")):
-			print ("Downloading pos.fea...")
-			urllib.urlretrieve ("http://clcl.unige.ch/btag/param/sr/pos.fea", "param/pos.fea")
+		if(not os.path.isfile(POS_WEIGHT)):
+			print ("Downloading POS weight file...")
+			urllib.urlretrieve ("http://clcl.unige.ch/btag/param/sr/pos.fea", POS_WEIGHT)
 			
-		if(not os.path.isfile("param/pos.scr")):
-			print ("Downloading pos.scr...")
-			urllib.urlretrieve ("http://clcl.unige.ch/btag/param/sr/pos.scr", "param/pos.scr")
+		if(not os.path.isfile(POS_SCRIPT)):
+			print ("Downloading POS script file...")
+			urllib.urlretrieve ("http://clcl.unige.ch/btag/param/sr/pos.scr", POS_SCRIPT)
 			
-		if(not os.path.isdir("tmp")):
-			call(["mkdir", "tmp"])
+		if(not os.path.isdir(TMP)):
+			call(["mkdir", TMP])
 			
 		# Run POS tagger
 		call("java -cp BTagger.jar bTagger/BTagger -p tmp/PosOut".split() + [inputFile,"param/pos.fea","param/pos.scr"])
@@ -64,6 +75,4 @@ if __name__ == '__main__':
 		
 		# Decode lemma tags
 		call("java  -cp BTagger.jar LCS_WDiff2L  tmp/LemmaOutTagged.txt".split() + [outputFile, "1", "3"])
-		
-		
 		
