99bfc761ca51eb288acc944aca8c96f48cdbe659
Aleksandar Milinkovic <amilinko@gmail.com>
2017-04-12 14:55:28 +0200

    Refactored stem.py. Replaced numbers with <NUM> tags.

--- stem.py
+++ stem.py
@@ -1,21 +1,49 @@
-from subprocess import call
-import sys
-import urllib
-import os
-import platform
+"""stem.py
+ 
+Usage:
+  stem.py <StemmerID> <InputFile> <OutputFile>
+  stem.py -h | --help
+  stem.py --version
+ 
+Options:
+  -h --help              Show this screen.
+  --version              Show version.
+ 
+"""
 
-# Parse command line arguments
-if len(sys.argv)<>4:
-	print "Usage: python stem.py <StemmerID> <InputFile> <OutputFile>"
-	sys.exit(1)
+from utils.docopt import docopt
 
-is_windows = True if platform.system()=="Windows" else False
-command = "java -jar SCStemmers.jar".split()
-arguments = sys.argv[1:]
-
-# Check if SCStemmers.jar is in the current directory
-if(not os.path.isfile("SCStemmers.jar")):
-	urllib.urlretrieve ("https://github.com/vukbatanovic/SCStemmers/releases/download/v1.0.0/SCStemmers.jar", "SCStemmers.jar")
+if __name__ == '__main__':
 	
-# Call Serbian stemmers jar from Python
-call(command + arguments, shell=is_windows)
+	arguments = docopt(__doc__, version="1.0.0")
+	
+	from subprocess import call
+	import urllib, os, platform, fileinput
+
+	# Parse command line arguments
+	stemmerID = arguments["<StemmerID>"]
+	inputFile = arguments["<InputFile>"]
+	outputFile = arguments["<OutputFile>"]
+	outputFileStem = outputFile + ".stem"
+	
+	NUM_TAG = "<NUM>"
+	
+	is_windows = True if platform.system()=="Windows" else False
+	command = "java -jar SCStemmers.jar".split()
+
+	# Check if SCStemmers.jar is in the current directory
+	if(not os.path.isfile("SCStemmers.jar")):
+		print ("Downloading SCStemmers.jar...")
+		urllib.urlretrieve ("https://github.com/vukbatanovic/SCStemmers/releases/download/v1.0.0/SCStemmers.jar", "SCStemmers.jar")
+	
+	# Call Serbian stemmers jar from Python
+	call(command + [stemmerID, inputFile, outputFileStem], shell=is_windows)
+
+	print ("Replacing numbers with <NUM> tags...")
+	output = open(outputFile, "w")
+	for line in fileinput.input(outputFileStem):
+		sentence = [NUM_TAG if word.isdigit() else word for word in line.split()]
+		output.write(" ".join(sentence))
+		output.write("\n")
+		
+	output.close()
