1c43d7f08edc59383af67c0d94668e610087cec4
Aleksandar Milinkovic <amilinko@gmail.com>
2017-07-09 19:33:26 +0200

    Parameterized filenames and assigned them as constants.

--- lemmatize.py
+++ lemmatize.py
@@ -53,22 +53,29 @@ if __name__ == '__main__':
         POS_WEIGHT = arguments["<PoS_fea>"]
         POS_SCRIPT = arguments["<PoS_scr>"]
         
+        # File names
+        POS_OUT = TMP + "/PosOut"
+        POS_OUT_TAGGED = TMP + "/PosOutTagged.txt"
+        LEMMA_OUT = TMP + "/LemmaOut"
+        LEMMA_OUT_TAGGED = TMP + "/LemmaOutTagged.txt"
+        LEMMAS_DECODED = TMP + "/decodedLemmas.txt"
+        
         # Check if Btagger.jar exists in the current directory
         if(not os.path.isfile(BTAGGER)):
             print ("Downloading BTagger.jar...")
             urllib.urlretrieve ("http://clcl.unige.ch/btag/BTagger.jar", BTAGGER)
             
         # Run POS tagger
-        call(("java -cp BTagger.jar bTagger/BTagger -p " + TMP + "/PosOut").split() + [inputFile,POS_WEIGHT,POS_SCRIPT], shell=is_windows)
+        call(("java -cp BTagger.jar bTagger/BTagger -p " + POS_OUT).split() + [inputFile,POS_WEIGHT,POS_SCRIPT], shell=is_windows)
         
         # Run lemmatizer
-        call(("java -cp BTagger.jar bTagger/BTagger -p " + TMP + "/LemmaOut").split() + [TMP + "/PosOutTagged.txt",LEMMA_WEIGHT,LEMMA_SCRIPT], shell=is_windows)
+        call(("java -cp BTagger.jar bTagger/BTagger -p " + LEMMA_OUT).split() + [POS_OUT_TAGGED, LEMMA_WEIGHT,LEMMA_SCRIPT], shell=is_windows)
         
         # Decode lemma tags
-        decodeBTaggerLemmaTags (TMP + "/LemmaOutTagged.txt", TMP + "/decodedLemmas.txt")
+        decodeBTaggerLemmaTags (LEMMA_OUT_TAGGED, LEMMAS_DECODED)
         
         # Parse decoded lemmatized input and prepare for vectorization
-        parseDecodedLemmas(TMP + "/decodedLemmas.txt", outputFile, filter_btagger)
+        parseDecodedLemmas(LEMMAS_DECODED, outputFile, filter_btagger)
     
     # CSTLemma
     if (arguments["CSTLemma"]):
@@ -84,25 +91,31 @@ if __name__ == '__main__':
             DICTIONARY = arguments["<Dictionary>"]
             PATTERNS = arguments["<Patterns>"]
             
+            # File names
+            POS_OUT_TAGGED = TMP + "/PosOutTaggedCST.txt"
+            POS_OUT_TAGGED_EOL = TMP + "/PosOutTaggedCST_EOL.txt"
+            LEMMAS_DECODED = TMP + "/CSTDecodedLemmas.txt"
+            LEMMAS_DECODED_CLEAN = TMP + "/CSTDecodedLemmasClean.txt"
+            
             # Run HunPos tagger
             print "Running HunPos tagger"
-            call("cat " + inputFile + " | hunpos-tag " + HUNPOS_SETTINGS + " > " + TMP + "/PosOutTaggedCST.txt" , shell=True)
+            call("cat " + inputFile + " | hunpos-tag " + HUNPOS_SETTINGS + " > " + POS_OUT_TAGGED , shell=True)
             
             # Adding <EOL> tags so sentences can be separated after CST lemmatizing
             print "Adding <EOL> tags"
-            call("cat " + TMP + "/PosOutTaggedCST.txt | sed -r 's/^$/<EOL>\t<EOL>\t/g' > " + TMP + "/PosOutTaggedCSTLines.txt", shell=True)
+            call("cat " + POS_OUT_TAGGED + " | sed -r 's/^$/<EOL>\t<EOL>\t/g' > " + POS_OUT_TAGGED_EOL, shell=True)
             
             # Run CST lemmatizer
             print "Running CST lemmatizer"
-            call(["cstlemma", "-L", "-i", TMP + "/PosOutTaggedCSTLines.txt", "-I", "$w\\t$t\\t\\n", "-d", DICTIONARY, "-f", PATTERNS, "-eU", "-o", TMP + "/CSTDecodedLemmas.txt", "-t", "-H0", "-u", "-U"])
+            call(["cstlemma", "-L", "-i", POS_OUT_TAGGED_EOL, "-I", "$w\\t$t\\t\\n", "-d", DICTIONARY, "-f", PATTERNS, "-eU", "-o", LEMMAS_DECODED, "-t", "-H0", "-u", "-U"])
             
             # Removing <EOL> tags
             print "Removing <EOL> tags"
-            call("cat " + TMP + "/CSTDecodedLemmas.txt | sed -r 's/<EOL>\t<EOL>\t<EOL>//g' > " + TMP + "/CSTDecodedLemmasClean.txt", shell=True)
+            call("cat " + LEMMAS_DECODED + " | sed -r 's/<EOL>\t<EOL>\t<EOL>//g' > " + LEMMAS_DECODED_CLEAN, shell=True)
             
             # Parse decoded lemmas
             print "Parsing decoded lemmas"
-            parseDecodedLemmas(TMP + "/CSTDecodedLemmasClean.txt", outputFile, filter_cst)
+            parseDecodedLemmas(LEMMAS_DECODED_CLEAN, outputFile, filter_cst)
             
     # ReLDI
     if (arguments["ReLDI"]):
